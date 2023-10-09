f190ca2352642f08ffe7a730fdfac70dbfa5972e
RESTORAN <007serzh2014@mail.ru>
2023-10-09 10:40:10 +0300

    start

--- game_store/src/App.js
+++ game_store/src/App.js
@@ -1,11 +1,22 @@
-
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import {HomePage} from './pages/home-page'
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      app
+      <Switch>
+        <Route path="/">
+          <HomePage/>
+          </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
